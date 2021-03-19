64da5170b7e56f8dc11b0062c236cdb2ea7f93bb
Silvio Junior <silvio_sbj@hotmail.com>
2021-03-18 21:13:01 -0400

    Setup project

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello!' });
+});
+
+app.listen(3333, () => console.info('Server is running!'));
