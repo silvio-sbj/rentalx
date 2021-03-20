5ee36ca8317e3eaea2c2581791e3c6f56ee2532a
Silvio Junior <silvio_sbj@hotmail.com>
2021-03-19 20:02:51 -0400

    Categories implementation

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello!' });
-});
+app.use('/categories', categoriesRoutes);
 
 app.listen(3333, () => console.info('Server is running!'));
